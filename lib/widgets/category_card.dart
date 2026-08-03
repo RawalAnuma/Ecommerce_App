1c6c4f92e881ca51f1ddffcdd62cca060ef305db
RawalAnuma <rawalanuma@gmail.com>
2026-08-03 19:51:27 +0545

    UI for displaying category

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/model/category_model.dart';
+import 'package:my_app/widgets/category_bottom_sheet.dart';
 
 class CategoryCard extends StatelessWidget {
   final CategoryModel category;
@@ -13,62 +14,71 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 220, 220, 220),
+    return InkWell(
+      borderRadius: BorderRadius.circular(18),
 
-        borderRadius: BorderRadius.circular(18),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.05),
-            blurRadius: 22,
-            offset: const Offset(0, 5),
-          ),
-        ],
-      ),
+      onTap: () {
+        CategoryBottomSheet.show(context, category);
+      },
 
-      child: Padding(
-        padding: const EdgeInsetsGeometry.all(14),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            CircleAvatar(
-              radius: 55,
-              backgroundColor: Colors.grey.shade100,
-              child: ClipOval(
-                child: Image.network(
-                  category.image,
-                  width: 90,
-                  height: 90,
-                  fit: BoxFit.cover,
-                  errorBuilder: (_, __, ___) {
-                    return const Icon(
-                      Icons.category,
-                      size: 45,
-                      color: Colors.grey,
-                    );
-                  },
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 8),
-
-            Text(
-              category.name,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-              textAlign: TextAlign.center,
-            ),
-
-            const SizedBox(height: 6),
-
-            Text(
-              "$productCount Items",
-              style: TextStyle(color: Colors.grey.shade600),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 220, 220, 220),
+          borderRadius: BorderRadius.circular(18),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.05),
+              blurRadius: 22,
+              offset: const Offset(0, 5),
             ),
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(14),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CircleAvatar(
+                radius: 55,
+                backgroundColor: Colors.grey.shade100,
+                child: ClipOval(
+                  child: Image.network(
+                    category.image,
+                    width: 90,
+                    height: 90,
+                    fit: BoxFit.cover,
+                    errorBuilder: (_, __, ___) {
+                      return const Icon(
+                        Icons.category,
+                        size: 45,
+                        color: Colors.grey,
+                      );
+                    },
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 8),
+
+              Text(
+                category.name,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 6),
+
+              Text(
+                "$productCount Items",
+                style: TextStyle(color: Colors.grey.shade600),
+              ),
+            ],
+          ),
         ),
       ),
     );
