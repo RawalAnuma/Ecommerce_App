ce9ae6e220980b22a567af6e0bf9b9f63b0cd525
RawalAnuma <rawalanuma@gmail.com>
2026-08-04 17:47:03 +0545

    Upload image method added

--- lib/services/category_service.dart
+++ lib/services/category_service.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
-
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:my_app/model/category_model.dart';
 
 class CategoryService {
   static const String url = "https://api.escuelajs.co/api/v1/categories";
+  static const String uploadUrl =
+      "https://api.escuelajs.co/api/v1/files/upload";
 
   //GET: Fetch all categories
   Future<List<CategoryModel>> fetchCategories() async {
@@ -64,6 +66,30 @@ class CategoryService {
     }
     throw Exception(
       "Failed to delete category: "
+      "${response.statusCode} ${response.body}",
+    );
+  }
+
+  Future<String> uploadImage(File file) async {
+    final request = http.MultipartRequest('POST', Uri.parse(uploadUrl))
+      ..files.add(await http.MultipartFile.fromPath('file', file.path));
+
+    final streamedResponse = await request.send();
+    final response = await http.Response.fromStream(streamedResponse);
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final data = jsonDecode(response.body);
+      final location = data['location'];
+      if (location == null || location.toString().isEmpty) {
+        throw Exception(
+          'Upload succeeded but API did not return an image location.',
+        );
+      }
+      //return data['location'];
+      return location.toString();
+    }
+    throw Exception(
+      "Failed to upload image: "
       "${response.statusCode} ${response.body}",
     );
   }
